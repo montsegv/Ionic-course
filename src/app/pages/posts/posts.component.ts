1bb642973563ccb0752f3c34e81843e07ab6fb32
Montserrat <maguilar14@ucol.mx>
2020-09-24 20:22:48 -0500

    Show POST in component

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -8,14 +8,18 @@ import { DataService } from '../../services/data.service';
 })
 export class PostsComponent implements OnInit {
 
+  mensajes: any;
+
   constructor( private dataService: DataService ) { }
 
   ngOnInit(): void {
 
-    this.dataService.getPosts()
-      .subscribe( posts => {
-        console.log(posts);
-    })
+    this.mensajes = this.dataService.getPosts()
+
+   //   .subscribe( (posts: any[]) => {
+   //     console.log(posts);
+   //     this.mensajes = posts;
+   // });
   }
 
 }
